339aa17a82397b17e91c0d4f4d787dfaaa900fc2
MoonXu0722 <xpylovely520@gmail.com>
2020-10-10 19:04:59 +0800

    20201010

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+﻿
+#include "player.h"
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#pragma comment(lib, "avformat.lib")
+#pragma comment(lib, "avcodec.lib")
+#pragma comment(lib, "avutil.lib")
+#pragma comment(lib, "swscale.lib")
+#pragma comment(lib, "SDL2.lib")
+
+int main(int argc, char *argv[]) {
+	try {
+		if (argc != 2) {
+			throw std::logic_error{"Not enough arguments"};
+		}
+
+		Player play{argv[1]};
+		play();
+	}
+
+	catch (const std::exception &e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return -1;
+	}
+
+	return 0;
+}
